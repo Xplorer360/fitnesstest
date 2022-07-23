b6b31b178940f9bdd2ec3c24e4b36a464026a4b7
Xplorer360 <107266773+Xplorer360@users.noreply.github.com>
2022-07-23 12:51:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background ="red";
+    event.target.style.color="white";
+});
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background ="pink";
+    event.target.style.color="white";
+});
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background ="purple";
+    event.target.style.color="white";
+});
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background ="orange";
+    event.target.style.color="white";
+});
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background ="magenta";
+    event.target.style.color="white";
+});
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background ="gold";
+    event.target.style.color="white";
+});
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background ="green";
+    event.target.style.color="white";
+});
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background ="grey";
+    event.target.style.color="white";
+});
+const change9 = document.getElementById("image9");
+
+change9.addEventListener("click", (event) => {
+    event.target.style.background ="indigo";
+    event.target.style.color="white";
+});
+const change10 = document.getElementById("image10");
+
+change10.addEventListener("click", (event) => {
+    event.target.style.background ="blue";
+    event.target.style.color="white";
+});
+const change11 = document.getElementById("image11");
+
+change11.addEventListener("click", (event) => {
+    event.target.style.background ="brown";
+    event.target.style.color="white";
+});
+
